5afcb030fddbb23a89f75440bef390fedd6b2f74
John Hill Escobar <johnhillescobar@users.noreply.github.com>
2023-06-07 18:56:44 -0500

    Config Update

---
+++ conf/config.py
@@ -0,0 +1,7 @@
+import os
+
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = int(os.environ.get('DB_PORT'))
+DB_NAME = os.environ.get('DB_NAME')
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
