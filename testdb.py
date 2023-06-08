e5a439353ac5986d9ba4616f80c73097d3839364
John Hill Escobar <johnhillescobar@users.noreply.github.com>
2023-06-07 20:14:40 -0500

    Update testdb.py

--- testdb.py
+++ testdb.py
@@ -2,12 +2,21 @@ import psycopg2
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from conf.config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
+import os
 
 from dotenv import load_dotenv
 
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT 
 
 from dotenv import load_dotenv
+
+
+print(DB_HOST)
+print(DB_PORT)
+print(DB_NAME)
+print(DB_USER)
+print(DB_PASSWORD)
+
 
 # Connect using psycopg2
 conn = psycopg2.connect(
@@ -17,6 +26,8 @@ conn = psycopg2.connect(
     user=DB_USER,
     password=DB_PASSWORD
 )
+
+
 
 # Create SQLAlchemy engine
 #engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
